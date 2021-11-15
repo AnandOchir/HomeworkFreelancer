43a4b3b7fcba71f86f2d63ef84286a6e5e17dddd
Anand-Ochir <anand-ochir@nest-56s-iMac.local>
2021-11-15 18:56:12 +0800

    Firebase setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useDoc } from './Hooks';
 
 const App = () => {
+  const { updateRecord } = useDoc('a/b')
+  const Send = () => {
+    updateRecord({
+      'test': 'c'
+    })
+  }
   return (
     <div>
+      <button onClick={Send} >
+        click me
+      </button>
       Test
     </div>
   );
